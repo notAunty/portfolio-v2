7c52f8b1330f393990336b63069bce8878ae3ea9
aso <wid180039@siswa.um.edu.my>
2022-02-14 16:53:54 +0800

    half working splash rotate effect

--- webpack/portfolio-splash.js
+++ webpack/portfolio-splash.js
@@ -1,8 +1,6 @@
 import config from "./config";
 export default () => {
-  $("#site").css("display", "none");
   setTimeout(() => {
     $("#portfolio-splash").css("display", "none");
-    $("#site").removeAttr("style");
-  }, config.production ? 2000 : 100);
+  }, config.production ? 2000 : 200000);
 }
