2437632e4cd744e5257ff0fccb119d9d7525fc9e
aso <wid180039@siswa.um.edu.my>
2022-02-14 20:06:47 +0800

    stoke offset animation

--- webpack/portfolio-splash.js
+++ webpack/portfolio-splash.js
@@ -1,6 +1,8 @@
 import config from "./config";
 export default () => {
-  setTimeout(() => {
-    $("#portfolio-splash").css("display", "none");
-  }, config.production ? 2000 : 200000);
+  // setTimeout(() => {
+  //   $("#portfolio-splash").css("display", "none");
+  // }, config.production ? 2000 : 200000);
+
+  // console.log($(".blob")[0].getTotalLength());
 }
